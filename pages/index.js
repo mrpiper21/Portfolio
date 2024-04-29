39f02ce9dc141924c729a21a12bcf73c56728643
mrpiper21 <125402164+mrpiper21@users.noreply.github.com>
2024-04-29 21:15:27 +0000

    update

--- pages/index.js
+++ pages/index.js
@@ -22,12 +22,6 @@ import A3 from "../public/Attendance/A3.jpg";
 import A4 from "../public/Attendance/A4.jpg";
 import A5 from "../public/Attendance/A5.jpg";
 import A6 from "../public/Attendance/A6.jpg";
-// import web1 from "../public/web1.png";
-// import web2 from "../public/web2.png";
-// import web3 from "../public/web3.png";
-// import web4 from "../public/web4.png";
-// import web5 from "../public/web5.png";
-// import web6 from "../public/web6.png";
 
 export default function Home() {
   const [darkMode, setDarkMode] = useState(false);
@@ -150,16 +144,13 @@ export default function Home() {
           <div>
             <h3 className="text-3xl py-1 dark:text-white ">Portofolio</h3>
             <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-              Since the beginning of my journey as a freelance designer and
-              developer, I have done remote work for
-              <span className="text-teal-500"> agencies </span>
-              consulted for <span className="text-teal-500">startups </span>
-              and collaborated with talanted people to create digital products
-              for both business and consumer use.
+              Since the beginning of my journey as a sofware developer, I have
+              collaborated with talanted people to create Apps for business and
+              customers.
             </p>
             <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-              I offer from a wide range of services, including brand design,
-              programming and teaching.
+              I offer from a wide range of services, including full pledge
+              professional website and mobite application.
             </p>
           </div>
 
