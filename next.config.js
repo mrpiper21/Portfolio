d1d5e107e16084e9b66a64deb3481e0dfd4aab80
mrpiper21 <125402164+mrpiper21@users.noreply.github.com>
2024-04-29 20:53:19 +0000

    update

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+};
+
+module.exports = {
+  images: {
+    domains: ["miro.medium.com"],
+  },
+};
+
+module.exports = nextConfig;
